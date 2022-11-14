c9a97d25e360425d9dd47f7d5bb981e48b75cb51
kvvivvic <rnjs9337@gmail.com>
2022-11-14 16:24:20 +0900

    파비콘 추가

--- html/detail_page.php
+++ html/detail_page.php
@@ -10,6 +10,8 @@ include "../inc/session.php";
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="favicon" href="./images/favicon.ico">
+  <link rel="shortcut icon" type="image/x-icon" href="../images/favicon.ico" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap" rel="stylesheet" />
